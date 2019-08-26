b6e59298eea0db8f0458d1111623eef47652bc45
Gavin Cabbage <gavincabbage@gmail.com>
2019-08-26 06:14:25 -0400

    cmd

--- cmd/stegosaurus/main.go
+++ cmd/stegosaurus/main.go
@@ -1,5 +1,64 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/urfave/cli"
+)
+
+var Version string
+
+func main() {
+	app := cli.App{
+		Name:        "stegosaurus",
+		HelpName:    "stegosaurus",
+		Version:     Version,
+		Description: "stegonography encoding and decoding tool",
+		Author:      "Gavin Cabbage",
+		Commands: []cli.Command{
+			{
+				Name:      "encode",
+				ShortName: "e",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:     "payload, p",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name:     "carrier, c",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name: "outfile, o",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					return nil
+				},
+			},
+			{
+				Name:      "decode",
+				ShortName: "d",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:     "data, d",
+						Required: true,
+					},
+					cli.StringFlag{
+						Name: "outfile, o",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					return nil
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.12
 
 require (
 	github.com/stretchr/testify v1.4.0
-	golang.org/dl v0.0.0-20190815200211-0d81d9315774 // indirect
+	github.com/urfave/cli v1.21.0
+	golang.org/dl v0.0.0-20190821202747-15c2a0da8b07 // indirect
 )
